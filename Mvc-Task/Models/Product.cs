b8c9c46f890d015eef6b3871efec5cb4fe76ba58
Snukii <ssnnuukkii@gmail.com>
2019-03-16 11:48:53 +0100

    Add project files.

---
+++ Mvc-Task/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Task.Models
+{
+    public class Product
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
---
+++ Mvc-Task/Models/ProductFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace Mvc_Task.Models
+{
+    public class ProductFactory : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        
+        public ProductFactory()
+        {
+            Database.SetInitializer(new ProductInitializer());
+        }
+    }
+
+    public class ProductInitializer : DropCreateDatabaseIfModelChanges<ProductFactory>
+    {
+        protected override void Seed(ProductFactory context)
+        {
+            context.Products.Add(new Product { Name = "Sour Dough", Color = "Grey", Price = 5 });
+            context.Products.Add(new Product { Name = "Full Grain", Color = "Brown", Price = 7.5 });
+        }
+    }
+}
